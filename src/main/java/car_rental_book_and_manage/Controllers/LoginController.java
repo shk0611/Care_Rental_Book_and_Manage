b7d97a2b7382529b6dfd19eacc6a04972bac7a2e
hshi270 <hshi270@aucklanduni.ac.nz>
2024-03-19 13:55:08 +1300

    implementing switching between accounts

--- src/main/java/car_rental_book_and_manage/Controllers/LoginController.java
+++ src/main/java/car_rental_book_and_manage/Controllers/LoginController.java
@@ -3,20 +3,72 @@ package car_rental_book_and_manage.Controllers;
 import car_rental_book_and_manage.App;
 import car_rental_book_and_manage.Utility.SceneManager;
 import car_rental_book_and_manage.Utility.SceneManager.Scenes;
+import car_rental_book_and_manage.Utility.SelectUser;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
-public class LoginController extends Controller{
-
-  public void initialize() {
-    SceneManager.setController(Scenes.LOGIN, this);
-  }
+public class LoginController extends Controller {
 
   @FXML private Button loginBtn;
 
+  @FXML private PasswordField password;
+
+  @FXML private ComboBox<String> selectUserComboBox;
+
+  @FXML private Button signUpBtn;
+
+  @FXML private TextField username;
+
+  public void initialize() {
+    SceneManager.setController(Scenes.LOGIN, this);
+    userSelect();
+  }
+
   @FXML
-  void onLogIn(MouseEvent event) {
+  public void switchLoginPortal() {
+    String selectedItem = selectUserComboBox.getSelectionModel().getSelectedItem();
+    if (selectedItem != null) {
+      switch (selectedItem) {
+        case "admin portal":
+          setPromptText("Admin");
+          break;
+        case "client portal":
+          setPromptText("Client");
+          break;
+        default:
+          // Handle other cases if needed
+          break;
+      }
+    }
+  }
+
+  private void setPromptText(String userRole) {
+    username.setPromptText(userRole + " Username");
+    password.setPromptText(userRole + " Password");
+  }
+
+  @FXML
+  public void onLogIn(MouseEvent event) {
     App.setUi(Scenes.HI);
+  }
+
+  public void userSelect() {
+
+    List<String> UserList = new ArrayList<>();
+
+    for (String user : SelectUser.users) {
+      UserList.add(user);
+    }
+
+    ObservableList<String> dataList = FXCollections.observableArrayList(UserList);
+    selectUserComboBox.setItems(dataList);
   }
 }
---
+++ src/main/java/car_rental_book_and_manage/Utility/SelectUser.java
@@ -0,0 +1,13 @@
+package car_rental_book_and_manage.Utility;
+
+import java.util.HashMap;
+
+public class SelectUser {
+
+    public static String[] users = {"admin portal","client portal"};
+
+    
+
+
+    
+}
